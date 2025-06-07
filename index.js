4a906d759154b01b2762c88efa0a7916aed25a4e
GABO <gumbaridzegabrieli@gmail.com>
2025-06-07 12:37:47 +0400

    initial commit

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 //1. დაწერეთ ფუნქცია რომელიც შეაბრუნებს სტრინგს
 const str = "Hello World!"; // !dlroW olleH
 const reverseString = (input) => {
-    return input.split('').reverse().join('');
+let reversed = '';
+for (let i = input.length - 1; i >= 0; i--) {
+    reversed += input[i];
+}
+return reversed;
 };
-
+/* Reverse function removed as per request */
 console.log(reverseString(str));
 //2. დაწერეთ ფუნქცია რომელიც სიტყვების მხოლოდ პირველ ასოებს გადააქცევს დიდ ასოებად.
 const str1 = "heLLo woRld"; // HeLLo woRLd
 const capitalizeFirstLetters = (input) => {
-    return input.split('').map((char, index) => {
+    return input.split('').map((lete, index) => {
         if (index === 0 || input[index - 1] === ' ') {
-            return char.toUpperCase();
+            return lete.toUpperCase();
         }
-        return char;
+        return lete;
     }).join('');
 }
 console.log(capitalizeFirstLetters(str1));
 //3. დაწერეთ ფუნქცია რომელიც მასივის შიდა ელემენტებს ამოკრეფს და გადააქცევს ერთ მთლიან მასივად
 const arr = [1, 2, 3, [4, 5], 6, [7, 8, 9]]; // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const mergeArrays = (input) => {
-    return input.reduce((let, num) => {
-        if (Array.isArray(num)) {
-            return let.concat(mergeArrays(num));
-        }
-        return let.concat(num);
-    }, []);
+    if  (Array.isArray(input)) {
+        return input.reduce((acc, val) => acc.concat(mergeArrays(val)), []);
+    }
+    return [input];
 }
 console.log(mergeArrays(arr));
 //4. დაწერეთ ფუნქცია რომელიც სტრინგს გადააქცევს მოკლე ჩანაწერად.
